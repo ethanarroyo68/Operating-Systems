c2c8505fbe3365b39cd84a1a25f2fbb98c447b3a
ethanarroyo68 <ethanarroyo68@gmail.com>
2025-02-06 18:03:03 -0700

    done

--- Ethan-Arroyo-HW1/MemoryManager.c
+++ Ethan-Arroyo-HW1/MemoryManager.c
@@ -2,32 +2,30 @@
 #include <stdlib.h>
 
 /**
- * Compares two numbers and returns a positive integer, 0 or 
+ * Compares two numbers and returns a positive integer, 0 or
  * a negative integer if the first number is greater than, equal to
  * or less than the second number.
  * Assumes the numbers are of the type int.
- * Used when invoking the quicksort function.  
+ * Used when invoking the quicksort function.
  */
-int compare (const void * a, const void * b)
+int compare(const void *a, const void *b)
 {
-   return (*(int*)a - *(int*)b);
+    return (*(int *)a - *(int *)b);
 }
 
-
 /**
- * Takes in an array of integers and the number of elements in the array 
+ * Takes in an array of integers and the number of elements in the array
  * as arguments.
- * Then it will sort and return the median of the sorted array. 
+ * Then it will sort and return the median of the sorted array.
  * To keep things simple, it always returns
- * the element at index arr_size/2 even though the 
+ * the element at index arr_size/2 even though the
  * array size is even.
  */
 int return_median(int *arr, int size)
 {
-   qsort(arr, size, sizeof(int), compare);
-   return arr[size/2];
+    qsort(arr, size, sizeof(int), compare);
+    return arr[size / 2];
 }
-
 
 /**
  * Takes in a random number and maps it to the
@@ -35,11 +33,11 @@ int return_median(int *arr, int size)
  */
 int get_iteration_count(int rand)
 {
-    return (rand % 20000) + 100000;     
+    return (rand % 20000) + 100000;
 }
 
 /**
- * Takes in a random number and maps it to the 
+ * Takes in a random number and maps it to the
  * range [1000 - 1500)
  */
 int get_arr_size(int rand)
@@ -47,19 +45,18 @@ int get_arr_size(int rand)
     return (rand % 500) + 1000;
 }
 
-
 /**
  * This is the method that you need to implement.
  * Implement the following steps in the given order.
  *
  *  1. Generate the number of iterations
- *      - Generate a random number. Map it to the range [100000 - 120000) using the 
+ *      - Generate a random number. Map it to the range [100000 - 120000) using the
  *      utility function 'get_iteration_count'.
- * 
+ *
  * 2. For each iteration:
  *      2.a Generate an array size - Generate a random number and map it to the range [1000 - 1500) using
  *          the utility function 'get_arr_size'.
- * 
+ *
  *      2.b Allocate memory for the array in the heap.
  *
  *      2.c Populate the array with random numbers.
@@ -71,7 +68,30 @@ int get_arr_size(int rand)
  *      2.f Return the number of medians that were divisible by 13.
  *
  */
-int get_running_count()
+int get_running_count(int running_count)
 {
     // TODO: Implement this method.
+    int *arr;
+    int iterations = get_iteration_count(rand());
+    int arrSize;
+    int mediansDivisible = 0;
+
+    for (int i = 0; i < iterations; i++)
+    {
+        arrSize = get_arr_size(rand());
+        arr = (int *)malloc(arrSize * sizeof(int));
+
+        for (int n = 0; n < arrSize; n++)
+        {
+            arr[n] = rand();
+        }
+
+        if ((return_median(arr, arrSize) % 13) == 0)
+        {
+            mediansDivisible++;
+        }
+        free(arr);
+    }
+    printf("[MemoryManager] Number of Iterations: %d\n", iterations);
+    return mediansDivisible;
 }
